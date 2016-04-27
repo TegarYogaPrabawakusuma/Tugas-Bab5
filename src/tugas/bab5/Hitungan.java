3a42ae17eb7e9448aecdf0668912701779edd00d
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-04-27 22:36:18 +0700

    percobaan final class hitungan

--- src/tugas/bab5/Hitungan.java
+++ src/tugas/bab5/Hitungan.java
@@ -30,6 +30,30 @@ public void Pembagian (int a, int b){
     int nilai=a/b;
     System.out.println("nilai pembagian adalah : "+nilai);
 }
+public void Sederhana(int a,int b){
+    int temp1 = a;
+       int temp2 = b; 
+
+        while (a != 0 && b != 0){
+     if(a > b){
+        a = a % b;
+     int n3 = temp1 / b ;
+      int n4 = temp2 / b ;
+
+     
+
+      System.out.println("Hasil Penyederhanaannya adalah "+n3 + "/" + n4 );
+     }else{
+        b = b % a;
+        int n3 = temp1 / a ;
+      int n4 = temp2 / a ;
+
+     
+
+      System.out.println("Hasil Penyederhanaannya adalah "+n3 + "/" + n4 );
+     }
+   }     
+    }     
     }     
   
 
