528c9372d91c8fc8bc6d587b06592211bdda1414
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-04-27 22:34:06 +0700

    percobaan pertama class hitungan

---
+++ src/tugas/bab5/Hitungan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LAPORAN;
+
+/**
+ *
+ * @author TEGAR
+ */
+public class Hitungan {
+ public static void Penjumlahan(int a,int b){
+int nilai = a+b;
+System.out.println("nilai penjumlahan adalah : "+nilai);
+
+}
+
+
+public static void Pengurangan(int a, int b){
+int nilai = a-b;
+System.out.println("nilai pengurangan adalah :"+nilai);
+}
+
+
+    }     
+  
+
+
+
